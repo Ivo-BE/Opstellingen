863de310713f6fe64cd53647e88184c2cfcf0fe7
Ivo-BE <ivo.van.ham@gmail.com>
2026-09-12 20:15:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Offline-cache voor de opstellingen-app. Verhoog het versienummer bij elke nieuwe index.html.
+const CACHE='opstellingen-v5';
+const FILES=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const sameOrigin=new URL(e.request.url).origin===location.origin;
+  if(sameOrigin){
+    // netwerk eerst (zodat updates binnenkomen), anders cache
+    e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  }else{
+    // lettertypes e.d.: cache eerst
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>new Response('',{status:503}))));
+  }
+});
